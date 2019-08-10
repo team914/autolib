ed18a252b2dcd92e0c19b9353797eda054ee56ab
potatehoes <acetousk@gmail.com>
2019-08-09 21:41:51 -0600

    :tada: logo :tada:

---
+++ include/okapi/api/chassis/controller/defaultOdomChassisController.hpp
@@ -0,0 +1,95 @@
+/**
+ * @author Ryan Benasutti, WPI
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include "okapi/api/chassis/controller/chassisControllerIntegrated.hpp"
+#include "okapi/api/chassis/controller/odomChassisController.hpp"
+#include "okapi/api/chassis/model/skidSteerModel.hpp"
+#include "okapi/api/odometry/odometry.hpp"
+#include <memory>
+
+namespace okapi {
+class DefaultOdomChassisController : public OdomChassisController {
+  public:
+  /**
+   * Odometry based chassis controller that moves using the V5 motor's integrated control. Spins up
+   * a task at the default priority plus 1 for odometry when constructed.
+   *
+   * Moves the robot around in the odom frame. Instead of telling the robot to drive forward or
+   * turn some amount, you instead tell it to drive to a specific point on the field or turn to
+   * a specific angle, relative to its starting position.
+   *
+   * @param itimeUtil The TimeUtil.
+   * @param imodel chassis model to use
+   * @param iodometry The odometry.
+   * @param ileftController left side controller
+   * @param irightController right side controller
+   * @param igearset The motor gearset.
+   * @param iscales The chassis scales.
+   * @param imoveThreshold minimum length movement (smaller movements will be skipped)
+   * @param iturnThreshold minimum angle turn (smaller turns will be skipped)
+   * @param ilogger The logger this instance will log to.
+   */
+  DefaultOdomChassisController(const TimeUtil &itimeUtil,
+                               std::unique_ptr<Odometry> iodometry,
+                               std::shared_ptr<ChassisController> icontroller,
+                               QLength imoveThreshold = 10_mm,
+                               QAngle iturnThreshold = 1_deg,
+                               std::shared_ptr<Logger> ilogger = Logger::getDefaultLogger());
+
+  /**
+   * Drives the robot straight to a point in the odom frame.
+   *
+   * @param ix x coordinate
+   * @param iy y coordinate
+   * @param ibackwards whether to drive to the target point backwards
+   * @param ioffset offset from target point in the direction pointing towards the robot
+   */
+  void driveToPoint(const QLength &ix,
+                    const QLength &iy,
+                    bool ibackwards = false,
+                    const QLength &ioffset = 0_mm) override;
+
+  /**
+   * Turns the robot to face an angle in the odom frame.
+   *
+   * @param iangle angle to turn to
+   */
+  void turnToAngle(const QAngle &iangle) override;
+
+  void moveDistance(QLength itarget) override;
+  void moveDistance(double itarget) override;
+  void moveDistanceAsync(QLength itarget) override;
+  void moveDistanceAsync(double itarget) override;
+  void turnAngle(QAngle idegTarget) override;
+  void turnAngle(double idegTarget) override;
+  void turnAngleAsync(QAngle idegTarget) override;
+  void turnAngleAsync(double idegTarget) override;
+  void setTurnsMirrored(bool ishouldMirror) override;
+  void waitUntilSettled() override;
+  void stop() override;
+  ChassisScales getChassisScales() const override;
+  AbstractMotor::GearsetRatioPair getGearsetRatioPair() const override;
+  std::shared_ptr<ChassisModel> getModel() override;
+  ChassisModel &model() override;
+
+  /**
+   * @return The internal chassis controller.
+   */
+  std::shared_ptr<ChassisController> getChassisController();
+
+  /**
+   * @return The internal chassis controller.
+   */
+  ChassisController &chassisController();
+
+  protected:
+  std::shared_ptr<Logger> logger;
+  std::shared_ptr<ChassisController> controller;
+};
+} // namespace okapi
