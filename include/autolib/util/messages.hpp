e121eb6d6a100ae04669e15c87cbe59a1c36da40
acetousk <acetousk@gmail.com>
2019-09-01 23:34:28 -0600

    :notebook_with_decorative_cover: update comment

--- include/autolib/util/messages.hpp
+++ include/autolib/util/messages.hpp
@@ -1,4 +1,12 @@
+/*
+ * @author D Michael Jones, 914M - acetousk
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 #pragma once
+
 #include "main.h"
 #include "okapi/api/units/QSpeed.hpp"
 #include "okapi/api/units/QAngle.hpp"
